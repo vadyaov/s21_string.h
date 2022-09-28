27de8e6cd0e9f6c692ef3aee3029c7def629b31d
vadim <vamyakish@mail.ru>
2022-09-28 20:52:37 +0300

    dasha trim good work

--- src/s21_trim.c
+++ src/s21_trim.c
@@ -1,35 +1,57 @@
 #include "s21_string.h"
 
+void delete_first_lex(char *str) {
+  s21_size_t count = 0;
+  while (str[count] != '\0') {
+    str[count] = str[count + 1];
+    count++;
+  }
+}
+
+void from_end(char *result, const char *src, const char *trim_chars) {
+  s21_size_t formatlen = s21_strlen(trim_chars);
+  int doBreak = 0, i = (int)s21_strlen(result) - 1;
+  while (i >= 0 && !doBreak) {
+    for (s21_size_t j = 0; j < formatlen; j++) {
+      if (result[i] == trim_chars[j]) {
+        result[i] = '\0';
+        break;
+      }
+      if (j == formatlen - 1 && src[i] != trim_chars[formatlen])
+        doBreak = 1;
+    }
+    i--;
+  }
+}
+
+void from_begin(char *result, const char *src, const char *trim_chars) {
+  s21_size_t formatlen = s21_strlen(trim_chars), strlen = s21_strlen(src),
+             i = 0;
+  int doBreak = 0;
+  while (!doBreak && i < strlen) {
+    for (s21_size_t j = 0; j < formatlen; j++) {
+      if (src[i] == trim_chars[j]) {
+        delete_first_lex(result);
+        break;
+      }
+      if (j == formatlen - 1 && src[i] != trim_chars[formatlen])
+        doBreak = 1;
+    }
+    i++;
+  }
+}
+
 void *s21_trim(const char *src, const char *trim_chars) {
-  char *buffer;
-  char *res = s21_NULL;
-  char *null_str = "";
-  char del[] = " \n\t\v\r\f";
-  if (trim_chars == s21_NULL || *trim_chars == *null_str) {
-    trim_chars = del;
-  }
+  char *result = s21_NULL;
   if (src) {
-    s21_size_t begin_trim_len;
-    begin_trim_len = s21_strspn(src, trim_chars);
-    src += begin_trim_len;
-    s21_size_t str_len = s21_strlen(src);
-    res = (char *)malloc(sizeof(char) * str_len + 1);
-    buffer = (char *)res;
-    while (*src) {
-      *res = *src;
-      res++;
-      src++;
+    if (!trim_chars || !s21_strlen(trim_chars))
+      trim_chars = " \t\n\v\r\f\0";
+    result = (char *)calloc(s21_strlen(src) + 13, sizeof(char));
+    if (result) {
+      s21_strcpy(result, (char *)src);
+      from_begin(result, src, trim_chars);
+      from_end(result, src, trim_chars);
     }
-    *res = '\0';
-    res--;
-    int ch = *res;
-    while (s21_strchr(trim_chars, ch) && str_len > 0) {
-      *res = '\0';
-      res--;
-      ch = (int)*res;
-      str_len--;
-    }
-    res = buffer;
   }
-  return (void *)res;
+  return (void *)result;
 }
--- src/tests/s21_trim_test.c
+++ src/tests/s21_trim_test.c
@@ -31,22 +31,151 @@ START_TEST(s21_trim_test3) {
     free(res);
 } END_TEST
 
+START_TEST(all_empty) {
+    char str[] = "";
+    char trim_ch[] = "";
+    char expected[] = "";
+    char *got = s21_trim(str, trim_ch);
+    ck_assert_str_eq(got, expected);
+    if (got)
+        free(got);
+}
+END_TEST
+
+START_TEST(str_empty) {
+    char str[] = "";
+    char trim_ch[] = "+!009876cjhloi;ixnauQFOUXHXIW---0902R____1R2";
+    char expected[] = "";
+    char *got = s21_trim(str, trim_ch);
+    ck_assert_str_eq(got, expected);
+    if (got)
+        free(got);
+}
+END_TEST
+
+START_TEST(trim_empty) {
+    char str[] = "+!009876cjhloi;ixnauQFOUXHXIW---0902R____1R2";
+    char trim_ch[] = "";
+    char expected[] = "+!009876cjhloi;ixnauQFOUXHXIW---0902R____1R2";
+    char *got = s21_trim(str, trim_ch);
+    ck_assert_str_eq(got, expected);
+    if (got)
+        free(got);
+}
+END_TEST
+
+START_TEST(trim_and_str_eq) {
+    char str[] = "+!009876cjhloi;ixnauQFOUXHXIW---0902R____1R2";
+    char trim_ch[] = "+!009876cjhloi;ixnauQFOUXHXIW---0902R____1R2";
+    char expected[] = "";
+    char *got = s21_trim(str, trim_ch);
+    ck_assert_str_eq(got, expected);
+    if (got)
+        free(got);
+}
+END_TEST
+
+START_TEST(trim_test1) {
+    char str[] = "+!!++Amo+++gu00s";
+    char trim_ch[] = "+!0-";
+    char expected[] = "Amo+++gu00s";
+    char *got = (char *)s21_trim(str, trim_ch);
+    ck_assert_str_eq(got, expected);
+    if (got)
+        free(got);
+}
+END_TEST
+
+START_TEST(trim_test2) {
+    char str[] = "Ab0030cd0";
+    char trim_ch[] = "003";
+    char expected[] = "Ab0030cd";
+    char *got = s21_trim(str, trim_ch);
+    ck_assert_str_eq(got, expected);
+    if (got)
+        free(got);
+}
+END_TEST
+
+START_TEST(trim_test3) {
+    char str[] = "DoNotTouch";
+    char trim_ch[] = "Not";
+    char expected[] = "DoNotTouch";
+    char *got = s21_trim(str, trim_ch);
+    ck_assert_str_eq(got, expected);
+    if (got)
+        free(got);
+}
+END_TEST
+
+START_TEST(trim_test4) {
+    char str[] = "&* !!scOoOOl21 * **";
+    char trim_ch[] = "&!* ";
+    char expected[] = "scOoOOl21";
+    char *got = s21_trim(str, trim_ch);
+    ck_assert_str_eq(got, expected);
+    if (got)
+        free(got);
+}
+END_TEST
+
+START_TEST(trim_test5) {
+    char str[] = " Good morning!    ";
+    char trim_ch[] = " ";
+    char expected[] = "Good morning!";
+    char *got = s21_trim(str, trim_ch);
+    ck_assert_str_eq(got, expected);
+    if (got)
+        free(got);
+}
+END_TEST
+
+START_TEST(empty_spaces) {
+    char str[] = "        abc         ";
+    char trim_ch[] = "";
+    char expected[] = "abc";
+    char *got = s21_trim(str, trim_ch);
+    ck_assert_str_eq(got, expected);
+    if (got)
+        free(got);
+}
+END_TEST
+
+START_TEST(null_ptr_trim_chars) {
+    char str[] = "        abc         ";
+    char *trim_ch = NULL;
+    char expected[] = "abc";
+    char *got = s21_trim(str, trim_ch);
+    ck_assert_str_eq(got, expected);
+    if (got)
+        free(got);
+}
+END_TEST
+
 Suite *s21_Suite_trim() {
     Suite *s;
 
     s = suite_create("Trim Unit Test");
 
-    TCase *tc1_s21_trim = tcase_create("test1_s21_trim");
-    TCase *tc2_s21_trim = tcase_create("test2_s21_trim");
-    TCase *tc3_s21_trim = tcase_create("test3_s21_trim");
+    TCase *tc = tcase_create("test1_s21_trim");
 
-    tcase_add_test(tc1_s21_trim, s21_trim_test1);
-    tcase_add_test(tc2_s21_trim, s21_trim_test2);
-    tcase_add_test(tc3_s21_trim, s21_trim_test3);
+    tcase_add_test(tc, s21_trim_test1);
+    tcase_add_test(tc, s21_trim_test2);
+    tcase_add_test(tc, s21_trim_test3);
+    tcase_add_test(tc, all_empty);
+    tcase_add_test(tc, str_empty);
+    tcase_add_test(tc, trim_empty);
+    tcase_add_test(tc, trim_and_str_eq);
 
-    suite_add_tcase(s, tc1_s21_trim);
-    suite_add_tcase(s, tc2_s21_trim);
-    suite_add_tcase(s, tc3_s21_trim);
+    tcase_add_test(tc, trim_test1);
+    tcase_add_test(tc, trim_test2);
+    tcase_add_test(tc, trim_test3);
+    tcase_add_test(tc, trim_test4);
+    tcase_add_test(tc, trim_test5);
+    tcase_add_test(tc, empty_spaces);
+    tcase_add_test(tc, null_ptr_trim_chars);
+
+    suite_add_tcase(s, tc);
 
     return s;
 }
